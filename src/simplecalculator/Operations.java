020c768fa564806dc312b62622088395d2bdd07d
adelmeleka <adelmeleka96@hotmail.com>
2018-01-25 03:27:03 -0800

    Add files via upload

---
+++ src/simplecalculator/Operations.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplecalculator;
+
+/**
+ *
+ * @author adelm
+ */
+public class Operations {
+
+    public int number1;
+    public int number2;
+    
+    
+   public int add(int numb1,int numb2){
+        int result = numb1 + numb2;
+        return result;
+    }
+
+}
